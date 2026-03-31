4c7ed491f73bee1cdfe1b9b4afa394971a338604
Ananddivine <ananddivine85@gmail.com>
2026-03-31 14:23:04 +1000

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,17 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react'; // Latest, with OXC built in
+import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    include: ['jwt-decode', 'jspdf'],
+    include: ["jwt-decode", "jspdf"]
   },
   assetsInclude: ['**/*.cur'],
   resolve: {
     extensions: ['.js', '.jsx', '.json'],
     alias: {
-      '@': path.resolve(__dirname, 'src'),
+      "@": path.resolve(__dirname, "src"),
     },
   },
 });
